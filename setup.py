b8f360838f094904053ed0245b2b982e92f8815d
Farbod Parkhuee <farbod.p1390@gmail.com>
2024-03-27 02:15:23 +0330

    update make_python function

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from colorama import init, Fore
 init()
-def make_exe(): pass
-def make_python(): pass
-try: input = int(input(Fore.GREEN + "write your telegram user id(with @userinfobot): "))
-except ValueError: print(Fore.RED + "You must write int."), exit()
+def make_exe(ID): 
+    make_python(ID=ID)
+def make_python(ID):
+    from os import system 
+    with open("MSTL/Files/info.info", "w") as writer: writer.write(ID)
+    print(Fore.GREEN + "Python file is ready in MSTL directory.")
+try: id = str(input(Fore.GREEN + "write your telegram user id(with @userinfobot): "))
+except ValueError: print(Fore.RED + "You must write str."), exit()
 state = str(input(Fore.GREEN + "For make a .exe file wirte EXE and for make a .py file write PY: ")).lower()
-if state == "exe": make_exe()
-else: make_python()
+if state == "exe": make_exe(ID=id)
+else: make_python(ID=id)
